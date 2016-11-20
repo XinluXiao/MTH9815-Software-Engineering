37f4cb0f7ed51621edca36eb89d07e210b02ad2c
Xinlu Xiao <xinluxiao.baruch@gmail.com>
2016-11-20 18:35:34 -0500

    add new Problem1.cpp

--- HW2/Problem1.cpp
+++ HW2/Problem1.cpp
@@ -9,7 +9,7 @@
 using namespace std;
 using namespace boost::interprocess;
 
-int main(int argc)
+int main(int argc,char *argv[])
 {
  if(argc == 1)
  {
@@ -25,19 +25,18 @@ int main(int argc)
   //See the output of the first program
   cout<<"The result of the first program is: "<<*i1<<endl;
   //Call the second program
-  main(0);
+  main(0,0);
   //Remove the shared memory object
   shared_memory_object::remove("SharedInt");
-  return 0;
  }
  else
  {
   //Create the second program
-  shared_memory_object(open_only, "SharedInt", read_only);
+  shared_memory_object shm(open_only, "SharedInt", read_only);
   //Map the whole shared memory in this process
   mapped_region region2(shm, read_only);
   int *i2 = static_cast<int*>(region2.get_address());
   cout<<"The result of the second program is: "<<*i2<<endl;
-  return 0;
  }
+ return 0;
 }
