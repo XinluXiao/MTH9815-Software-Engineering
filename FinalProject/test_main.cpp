8c872b3c0f7bfceba615dc0b31048d540163fbd3
Xinlu Xiao <xinluxiao.baruch@gmail.com>
2016-12-09 23:07:25 -0500

    create Final Project file

---
+++ FinalProject/test_main.cpp
@@ -0,0 +1,19 @@
+/**
+ * test_main.cpp
+ * Test files
+ * @ Create by Xinlu Xiao
+ */
+
+#include <iostream>
+#include <fstream>
+#include <random>
+#include <string>
+#include "tradeFileGenerator.hpp"
+
+using namespace std;
+
+int main()
+{
+   trade_generate();
+   return 0;
+}
